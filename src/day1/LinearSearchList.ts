ffa68a8222b41f0498958c7156490f50ee35469f
Vishnu Pasuleti <vpasuleti@compass.education>
2023-04-26 14:51:33 +0530

    Day 1 setup

---
+++ src/day1/LinearSearchList.ts
@@ -0,0 +1,7 @@
+export default function linear_search(haystack: number[], needle: number): boolean {
+    for (let index = 0; index < haystack.length; index++) {
+        if (haystack[index] === needle) return true;        
+    }
+
+    return false;
+}
