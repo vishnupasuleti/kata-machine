d2cbb2f7e55a4e6cbe637c0e5988352af9328c4a
Vishnu Pasuleti <vpasuleti@compass.education>
2023-05-15 21:26:18 +0530

    Queue implementation

--- src/__tests__/Queue.ts
+++ src/__tests__/Queue.ts
@@ -3,21 +3,21 @@ import Queue from "@code/Queue";
 test("queue", function () {
     const list = new Queue<number>();
 
-    list.enqueue(5);
-    list.enqueue(7);
-    list.enqueue(9);
+    list.enqueue(5);//5
+    list.enqueue(7);//5->7
+    list.enqueue(9);//5-7-9
 
-    expect(list.deque()).toEqual(5);
+    expect(list.deque()).toEqual(5); // 7->9
     expect(list.length).toEqual(2);
 
     // this must be wrong..?
     debugger;
 
     // i hate using debuggers
-    list.enqueue(11);
+    list.enqueue(11); // 7->9->11
     debugger;
-    expect(list.deque()).toEqual(7);
-    expect(list.deque()).toEqual(9);
+    expect(list.deque()).toEqual(7); //9->11
+    expect(list.deque()).toEqual(9); // 11
     expect(list.peek()).toEqual(11);
     expect(list.deque()).toEqual(11);
     expect(list.deque()).toEqual(undefined);
--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,42 @@
+type QNode<T> = {
+    value: T;
+    next?: QNode<T>;
+};
 export default class Queue<T> {
     public length: number;
-
-    
+    public head?: QNode<T>;
+    public tail?: QNode<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        const node: QNode<T> = { value: item };
+        ++this.length;
+        if (!this.tail) {
+            this.head = this.tail = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+        --this.length;
+        const head = this.head;
+        this.head = this.head.next;
+        head.next = undefined;
+        if (this.length === 0) this.tail = undefined;
+        return head.value;
+    }
 
-}
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
